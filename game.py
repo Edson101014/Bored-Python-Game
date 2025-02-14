b9bc988c3e7d011d327104fcc84022aff82ee820
Edson <edsonpaul98@gmail.com>
2025-02-14 16:25:14 +0800

    -update screen size snake game

--- game.py
+++ game.py
@@ -11,8 +11,8 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-dis_width = 200
-dis_height = 200
+dis_width = 500
+dis_height = 500
 
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by GitHub Copilot')
